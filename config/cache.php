498a7d32176fe438ccd6ce96cf0aecdf4c5e86d0
DoWake <298666015@qq.com>
2023-11-04 21:21:18 +0800

    配置Redis

--- config/cache.php
+++ config/cache.php
@@ -24,6 +24,16 @@ return [
             // 序列化机制 例如 ['serialize', 'unserialize']
             'serialize'  => [],
         ],
+        'redis'   =>  [
+            // 驱动方式
+            'type' => 'redis',
+            // 服务器地址
+            'host' => env('redis.host', '127.0.0.1'),
+            // 端口
+            'port' => env('redis.port', '6379'),
+            // 密码
+            'password' => env('redis.password', ''),
+        ],
         // 更多的缓存连接
     ],
 ];
